0d8bd39a02f902269d8bd5b4da471559490ce31c
bingphuong <86761953+bingphuong@users.noreply.github.com>
2021-07-01 12:28:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'condi của em ơii!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnIntro: 'hjhj',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên=)))',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là ju em <33',
+    btnNo: 'Em bị ảo à',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em z bae',
+    btnReply: 'send nude <3',
+    reply: 'vì em cute vl với lại anh bị thỉu năng nữa :3',
+    mess: 'ngại ghê cơ >//< em cũng yêu bbi:3',
+    messDesc: 'thích thì ib không thích thì cũng cố mà ib mau lên 🤬.',
+    btnAccept: 'dza dza',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
